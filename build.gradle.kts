d8d5e64488fd23b1cf08312702763ed0b6a731b8
Sindre Moen <sindrenm@gmail.com>
2025-12-02 15:05:33 +0100

    List all applied plugins in root build script

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.convention.android.application) apply false
+  alias(libs.plugins.convention.android.library) apply false
+  alias(libs.plugins.convention.jvm.library) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.kotlin.ksp) apply false
+  alias(libs.plugins.kotlin.serialization) apply false
+  alias(libs.plugins.metro) apply false
+}
